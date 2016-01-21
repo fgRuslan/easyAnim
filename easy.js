0a19f907a89e896037313efcd0d2ecebdf597b8e
Frostgate <rusikisaev@list.ru>
2016-01-21 19:32:50 +0300

    Added  rotate  function

--- easy.js
+++ easy.js
@@ -65,16 +65,65 @@ $(obj).css("z-index",indx);
 //V2.0
 function makeAttention(obj,time)
 {
-	$(obj).css("position","relative");
-	$(obj).animate({opacity: "hide", top: "-10"},0);
-	$(obj).animate({opacity: "show", top: "10"}, "slow");
-	setTimeout(function()
-	{
-	$(obj).animate({opacity: "hide", top: "-10"}, "slow");
-	},time);
+translate(obj,$(obj).position(),$(obj).position()+10,"slow");
+setTimeout(function(){
+	translate(obj,$(obj).position(),$(obj).position()-10,"slow");
+},time);
 }
 function translate(obj,x,y,time)
 {
-$(obj).css("position","relative");//make  it  movable
-$(obj).animate({left:x,top:y},time);//move
+$(obj).css("position","relative");
+$(obj).animate({left:x,top:y},time);
+}
+function scale(obj,width1,height1,time)
+{
+$(obj).animate({
+width:width1,height:height1}
+,time);
+}
+//Add  here  more  arguments
+function fullAnim(obj,width1,opacity1,marginLeft1,fsize,bw1,height1,bh1,visib,pos,bottom1,top1,TIME)
+{
+$(obj).animate({
+    width: width1,
+    opacity: opacity1,
+    marginLeft: marginLeft1,
+    fontSize: fsize,
+    borderWidth: bw1,
+	height:height1,
+	borderHeight:bh1,
+	position:pos,
+	visibility:visib,
+	bottom:bottom1,
+	top:top1
+  }, TIME );
+}
+function rotate(obj,deg1,time)
+{/*yeah  yeah  i  know*/
+/*  It's  not  working  without  fullAnim  function..  I  don't  know  why..*/
+/* TODO: Fix  this  bug*/
+$(obj).animate({
+    "opacity": "1"
+}, 1, function () {
+    $(obj).animate({
+        "position":"relative"
+    }, 1, function () {
+        $({
+            deg: 0
+        }).animate({
+            deg: deg1
+        }, {
+            duration: time,
+            step: function (now) {
+                $(obj).css({
+                    transform: 'rotate(' + now + 'deg)'
+                });
+            }
+        });
+    });
+});
+
+/*$("#object").css({
+transform: 'rotate(' + 90 + 'deg)'
+});*/
 }
