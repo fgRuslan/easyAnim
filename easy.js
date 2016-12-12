818899be9492227be0fb06676ad4a9a16e5b54d9
Ruslan <rusikisaev@list.ru>
2016-12-12 16:30:42 +0300

    Remove  anim  and  fullAnim  functions

--- easy.js
+++ easy.js
@@ -35,18 +35,6 @@ easy.makeDraggable=function(obj)
 {
 $(obj).draggable();
 }
-easy.anim=function(obj,width1,opacity1,marginLeft1,fsize,borderWidth1,height1,bh1,TIME)
-{
-$(obj).animate({
-    width: width1,
-    opacity: opacity1,
-    marginLeft: marginLeft1,
-    fontSize: fsize,
-    borderWidth: borderWidth1,
-	height:height1,
-	borderHeight:bh1
-  }, TIME );
-}
 easy.fadeOut=function(obj,time)
 {
 $(obj).animate(
@@ -91,27 +79,6 @@ easy.scale=function(obj,width1,height1,time)
 $(obj).animate({
 width:width1,height:height1}
 ,time);
-}
-easy.fullAnim=function(obj,width1,opacity1,marginLeft1,fsize,bw1,height1,width1,bh1,visib,pos,bottom1,top1,bottom1,left1,right1,TIME)
-{
-$(obj).css("position","relative");
-$(obj).animate({
-    width: width1,
-    opacity: opacity1,
-    marginLeft: marginLeft1,
-    fontSize: fsize,
-    borderWidth: bw1,
-	height:height1,
-	width:width1,
-	borderHeight:bh1,
-	position:pos,
-	visibility:visib,
-	bottom:bottom1,
-	top:top1,
-	bottom:bottom1,
-	left:left1,
-	right:right1
-  }, TIME );
 }
 easy.rotate=function(obj,deg1,time)
 {
